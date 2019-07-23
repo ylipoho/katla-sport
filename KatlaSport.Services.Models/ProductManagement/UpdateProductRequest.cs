5567b5267ed8de297ecace05a34430cf8cba3ee2
Lizaveta Baranouskaya <lizaveta_baranouskaya@epam.com>
2019-07-23 19:41:58 +0300

    Add Description, ManufacturerCode and Price property to CatalogueProduct and UpdateProductRequest model. Create a new migration AddProductDescriptionManCodePrice. Add new rules to UpdateProductRequestValidator.

--- KatlaSport.Services.Models/ProductManagement/UpdateProductRequest.cs
+++ KatlaSport.Services.Models/ProductManagement/UpdateProductRequest.cs
@@ -24,5 +24,20 @@ namespace KatlaSport.Services.ProductManagement
         public int CategoryId { get; set; }
 
         // TODO STEP 2 - Add "Description", "ManufacturerCode" and "Price" properties here.
+
+        /// <summary>
+        /// Gets or sets a product description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets a product manufacturer code.
+        /// </summary>
+        public string ManufacturerCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets a product price.
+        /// </summary>
+        public decimal Price { get; set; }
     }
 }
