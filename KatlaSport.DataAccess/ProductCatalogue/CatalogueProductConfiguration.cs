a0ae6c5a978a42aedcdadd936644ad4c014772af
Lizaveta Baranouskaya <lizaveta_baranouskaya@epam.com>
2019-07-23 19:45:05 +0300

    Merge step2. Add Description, ManufacturerCode and Price property to CatalogueProduct and UpdateProductRequest model. Create a new migration AddProductDescriptionManCodePrice. Add new rules to UpdateProductRequestValidator.

--- KatlaSport.DataAccess/ProductCatalogue/CatalogueProductConfiguration.cs
+++ KatlaSport.DataAccess/ProductCatalogue/CatalogueProductConfiguration.cs
@@ -11,6 +11,9 @@ namespace KatlaSport.DataAccess.ProductCatalogue
             HasKey(i => i.Id);
             HasIndex(i => i.Code);
             HasRequired(i => i.Category).WithMany(i => i.Products).HasForeignKey(i => i.CategoryId);
+            Property(i => i.Description).HasColumnName("product_description").HasMaxLength(300);
+            Property(i => i.ManufacturerCode).HasColumnName("product_manufacturer_code").HasMaxLength(10);
+            Property(i => i.Price).HasColumnName("product_price").IsOptional();
             Property(i => i.Id).HasColumnName("product_id").HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
             Property(i => i.Name).HasColumnName("product_name").HasMaxLength(60).IsRequired();
             Property(i => i.Code).HasColumnName("product_code").HasMaxLength(5).IsRequired();
